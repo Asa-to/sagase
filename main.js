ae85888a9fa01547362931c15104c7c7641192c6
Asa-to <sethuna03@gmail.com>
2023-11-30 15:56:05 +0900

    update

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 const element = document.getElementById("waldo");
-element.style.display = "block";
-
-const init = () => {
-  update();
-  element.style.display = "block";
-};
+const fps = 120;
+const startTime = new Date().getTime();
+const positionLeft = window.parent.screen.width * Math.random();
+const positionTop = window.parent.screen.height * Math.random();
 
 const update = () => {
   // ウィンドウ端からの相対距離をモニタ端からの相対距離に直して、その後配置したい位置に合わせる式
-  const left = -window.screenLeft + window.parent.screen.width / 2;
-  const top = -window.screenTop + window.parent.screen.height / 2;
-  console.log(window.screenTop);
+  const left = -window.screenLeft + positionLeft;
+  const top = -window.screenTop + positionTop;
   element.style.left = left + "px";
   element.style.top = top + "px";
 };
 
+const init = () => {
+  update();
+  element.onclick = () => {
+    const clearTime = (new Date().getTime() - startTime) / 1000;
+    alert(`${clearTime}秒で見つけられました！`);
+    location.reload();
+  };
+  element.style.display = "block";
+};
+
 init();
-setInterval(update, 1000 / 60);
+setInterval(update, 1000 / fps);
